ff066ca7d507d4d25a9785d215a413fa3a7bc1c7
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-06-10 00:04:02 +0530

    sockets

--- Frontend/src/utils/socket.ts
+++ Frontend/src/utils/socket.ts
@@ -1,8 +1,8 @@
-import { io } from 'socket.io-client'
+import { io } from "socket.io-client";
 
 console.log("Connecting socket to:", import.meta.env.VITE_API_URL);
 
 export const socket = io(import.meta.env.VITE_API_URL, {
-    withCredentials: true,
-    transports: ['websocket'],
+  withCredentials: true,
+  transports: ["websocket"],
 });
