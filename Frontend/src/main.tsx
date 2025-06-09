9384646a27b0262a704fe27546363d511977cfcc
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-06-09 23:58:23 +0530

    document

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { Provider } from 'react-redux';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
 import store, { persistor } from "./redux/store/store.ts";
-import { PersistGate } from 'redux-persist/es/integration/react';
+import { PersistGate } from "redux-persist/es/integration/react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-const googleClient = import.meta.env.VITE_GOOGLE_CLIENT_ID
+const googleClient = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
