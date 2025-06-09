348a04b2b26d081cc7ba9a1a0d6efe3d0685cbf4
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-06-09 23:49:47 +0530

    Media error

--- Frontend/src/api/media/mediaApi.ts
+++ Frontend/src/api/media/mediaApi.ts
@@ -1,22 +1,17 @@
 import { axiosInstance } from "../axios/axiosInstance";
 
 export const uploadChatMedia = async (file: File) => {
-    try {
-        const formData = new FormData();
-        formData.append("media", file);
+  try {
+    const formData = new FormData();
+    formData.append("media", file);
 
-        console.log('FORMDATA in axios', formData);
-        
+    console.log("FORMDATA in axios", formData);
 
-        const response = await axiosInstance.post(
-            `/api/media/upload`,
-            formData,
-            {
-                headers: { "Content-Type": "multipart/form-data" },
-            }
-        );
-        return response.data;
-    } catch (error: any) {
-        throw error.response?.data || "Failed to upload media";
-    }
+    const response = await axiosInstance.post(`/api/media/upload`, formData, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    return response.data;
+  } catch (error: any) {
+    throw error.response?.data || "Failed to upload media";
+  }
 };
