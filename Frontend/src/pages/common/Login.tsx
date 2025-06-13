9e0db86040e57559ed23c3e8b3edbbda9e973416
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-06-13 21:59:53 +0530

    login structure

--- Frontend/src/pages/common/Login.tsx
+++ Frontend/src/pages/common/Login.tsx
@@ -2,12 +2,12 @@ import LoginForm from "@/components/login/LoginForm";
 import LoginNav from "@/components/login/LoginNav";
 
 const Login = () => {
-    return (
-        <div className="min-h-screen bg-gray-100 dark:bg-gray-950">
-            <LoginNav/>
-            <LoginForm/>
-        </div>
-    );
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-950">
+      <LoginNav />
+      <LoginForm />
+    </div>
+  );
 };
 
 export default Login;
