4458b7816e5182cf5c19c629cf3cd61066f38fda
Harsh M <52977247+harshmriduhash@users.noreply.github.com>
2025-06-13 22:02:06 +0530

    signup

--- Frontend/src/pages/common/SignUp.tsx
+++ Frontend/src/pages/common/SignUp.tsx
@@ -2,12 +2,12 @@ import SignUpForm from "@/components/signup/SignUpForm";
 import SignUpNav from "@/components/signup/SignupNav";
 
 const SignUp = () => {
-    return (
-        <div className="min-h-screen bg-gray-100 dark:bg-gray-950">
-            <SignUpNav />
-            <SignUpForm />
-        </div>
-    )
+  return (
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-950">
+      <SignUpNav />
+      <SignUpForm />
+    </div>
+  );
 };
 
-export default SignUp
+export default SignUp;
